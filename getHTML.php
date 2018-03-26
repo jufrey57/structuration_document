1e43fbefb1c7983af5f3d8980d5b6c3d54159e93
Julien F <jufrey57@gmail.com>
2018-03-26 09:30:44 +0200

    ajout fonction getRawHTML

--- getHTML.php
+++ getHTML.php
@@ -1,2 +1,18 @@
 <?
 
+function getRawHTML() {
+	ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)'); 
+
+	$optionsContext = array(
+		'http'=>array(
+			'method'=>"GET",
+			'header'=>"Content-type: text/html"
+		)
+	);
+
+	$context = stream_context_create($optionsContext);
+
+	$rawHTML = file_get_contents('http://wonderworldweb.over-blog.com/', false, $context);
+
+	print_r($rawHTML);
+}
